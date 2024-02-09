82d38ec6e035371002fc13b212f5ecb44ac3557f
Mattias Axelsson <yhma23.axelma@folkuniversitetet.nu>
2024-02-09 15:34:18 +0100

    Updated user class to be inline with user stories

--- app/src/main/java/com/example/turbo_transport/User.kt
+++ app/src/main/java/com/example/turbo_transport/User.kt
@@ -4,11 +4,18 @@ import com.google.firebase.Timestamp
 import com.google.firebase.firestore.DocumentId
 import com.google.firebase.firestore.ServerTimestamp
 
-data class User(
+ data class User(
     @DocumentId
     var documentId : String? = null,
     var userId: String? = null,
-    var userName : String? = null,
+    var name : String? = null,
+    var level : Int? = null, //1 = customer, 2 = driver, 3 = admin?
+    var userAddress : String? = null,
+    var userPhoneNumber : String? = null,
+    var approveNotifications : Boolean? = null,
+    var vehicleId : String? = null,
+    @ServerTimestamp
+    var lastEdited: Timestamp? = null,
     @ServerTimestamp
     var timestamp: Timestamp? = null) {
 }
