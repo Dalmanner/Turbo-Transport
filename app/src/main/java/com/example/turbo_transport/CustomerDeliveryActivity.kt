dd5797bc3ed416e2affd87b949c6e0692c61dafc
Mattias Axelsson <yhma23.axelma@folkuniversitetet.nu>
2024-02-15 23:57:30 +0100

    go back to listdeliveries and clear stack by pressing help

--- app/src/main/java/com/example/turbo_transport/CustomerDeliveryActivity.kt
+++ app/src/main/java/com/example/turbo_transport/CustomerDeliveryActivity.kt
@@ -1,11 +1,41 @@
 package com.example.turbo_transport
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.appbar.AppBarLayout
+import com.google.android.material.appbar.MaterialToolbar
 
 class CustomerDeliveryActivity : AppCompatActivity() {
+
+    private lateinit var appBarLayout: AppBarLayout
+    private lateinit var topAppBar: MaterialToolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_customer_delivery)
+
+        initializeViews()
+        showMenu()
+    }
+    private fun showMenu(){
+        topAppBar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.user -> {
+                    // Handle edit text press
+                    true
+                }
+                R.id.help -> {
+                    val intent = Intent(this, ListDeliveries::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
+                    startActivity(intent)
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+    private fun initializeViews() {
+        appBarLayout = findViewById(R.id.appBarLayout)
+        topAppBar = findViewById(R.id.topAppBar)
     }
 }
