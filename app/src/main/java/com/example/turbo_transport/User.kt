3966712d7df12563085674f9ff7c4bd677a371bb
alvinjhson <alvinbest26@gmail.com>
2024-03-04 17:39:18 +0100

    created a profile for receiver with log out button

--- app/src/main/java/com/example/turbo_transport/User.kt
+++ app/src/main/java/com/example/turbo_transport/User.kt
@@ -11,10 +11,13 @@ import com.google.firebase.firestore.ServerTimestamp
     var name : String? = null,
     var level : Int? = null, //1 = customer, 2 = driver, 3 = admin?
     var userAddress : String? = null,
+    var cityName : String? = null,
+    var postCode : String? = null,
     var userPhoneNumber : String? = null,
     var approveNotifications : Boolean? = null,
     var vehicleId : String? = null,
     var notificationToken: String? = null,
+    var Email: String? = null,
     @ServerTimestamp
     var lastEdited: Timestamp? = null,
     @ServerTimestamp
