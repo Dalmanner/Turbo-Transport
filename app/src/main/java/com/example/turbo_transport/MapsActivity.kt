30218c587a94deea116ebb4d4ccf23fffbd6dbb6
Dalmanner <145015597+Dalmanner@users.noreply.github.com>
2024-03-05 10:53:24 +0100

    Code cleanup

--- app/src/main/java/com/example/turbo_transport/MapsActivity.kt
+++ app/src/main/java/com/example/turbo_transport/MapsActivity.kt
@@ -51,21 +51,11 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
         bottomMenu()
         db = Firebase.firestore
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
@@ -187,16 +177,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
             LocationServices.getFusedLocationProviderClient(this)
         currentUserLocation.lastLocation.addOnSuccessListener { location ->
 
-
             if (location != null) {
                 val startLatLng = LatLng(location.latitude, location.longitude)
                 endLatLng?.let { setMarkersAndRoute(startLatLng, it) }
             }
         }
-
-
         mMap.isMyLocationEnabled = true
-
     }
 
 
